067d958cf2aaa0b772ae3adee2cf5e46a9adda93
isaac lee <isaac312@ucla.edu>
2020-08-15 12:37:49 -0700

    Final Check

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import Customer from './components/Customer';
 import CustomerAdd from './components/CustomerAdd';
 import './App.css';
@@ -9,13 +11,17 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { withStyles } from '@material-ui/core/styles';
+import {
+  withStyles
+} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-import { fade } from '@material-ui/core/styles/colorManipulator';
+import {
+  fade
+} from '@material-ui/core/styles/colorManipulator';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
@@ -114,14 +120,18 @@ class App extends Component {
       searchKeyword: ''
     });
     this.callApi()
-      .then(res => this.setState({customers: res}))
+      .then(res => this.setState({
+        customers: res
+      }))
       .catch(err => console.log(err));
   }
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
     this.callApi()
-      .then(res => this.setState({customers: res}))
+      .then(res => this.setState({
+        customers: res
+      }))
       .catch(err => console.log(err));
   }
 
@@ -132,8 +142,12 @@ class App extends Component {
   }
 
   progress = () => {
-    const { completed } = this.state;
-    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+    const {
+      completed
+    } = this.state;
+    this.setState({
+      completed: completed >= 100 ? 0 : completed + 1
+    });
   }
 
   handleValueChange = (e) => {
@@ -148,64 +162,150 @@ class App extends Component {
         return c.name.indexOf(this.state.searchKeyword) > -1;
       });
       return data.map((c) => {
-        return <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> 
+        return <Customer stateRefresh = {
+          this.stateRefresh
+        }
+        key = {
+          c.id
+        }
+        id = {
+          c.id
+        }
+        image = {
+          c.image
+        }
+        name = {
+          c.name
+        }
+        birthday = {
+          c.birthday
+        }
+        gender = {
+          c.gender
+        }
+        job = {
+          c.job
+        }
+        /> 
       });
     }
-    const { classes } = this.props;
+    const {
+      classes
+    } = this.props;
     const cellList = ["번호", "프로필 이미지", "이름", "생년월일", "성별", "직업", "설정"];
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
-              <MenuIcon />
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
-              고객 관리 시스템
-            </Typography>
-            <div className={classes.grow} />
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="검색하기"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                name="searchKeyword"
-                value={this.state.searchKeyword}
-                onChange={this.handleValueChange}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
-        <div className={classes.menu}>
-          <CustomerAdd stateRefresh={this.stateRefresh}/>
-        </div>
-        <Paper className={classes.paper}>
-          <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                {cellList.map(c => {
-                  return <TableCell className={classes.tableHead}>{c}</TableCell>
-                })}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.customers ? 
-                filteredComponents(this.state.customers) :
-              <TableRow>
-                <TableCell colSpan="6" align="center">
-                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-                </TableCell>
-              </TableRow>
-              }
-            </TableBody>
-          </Table>
-        </Paper>
-      </div>
+    return ( <
+      div className = {
+        classes.root
+      } >
+      <
+      AppBar position = "static" >
+      <
+      Toolbar >
+      <
+      IconButton className = {
+        classes.menuButton
+      }
+      color = "inherit"
+      aria - label = "Open drawer" >
+      <
+      MenuIcon / >
+      <
+      /IconButton> <
+      Typography className = {
+        classes.title
+      }
+      variant = "h6"
+      color = "inherit"
+      noWrap >
+      고객 관리 시스템 <
+      /Typography> <
+      div className = {
+        classes.grow
+      }
+      /> <
+      div className = {
+        classes.search
+      } >
+      <
+      div className = {
+        classes.searchIcon
+      } >
+      <
+      SearchIcon / >
+      <
+      /div> <
+      InputBase placeholder = "검색하기"
+      classes = {
+        {
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }
+      }
+      name = "searchKeyword"
+      value = {
+        this.state.searchKeyword
+      }
+      onChange = {
+        this.handleValueChange
+      }
+      /> <
+      /div> <
+      /Toolbar> <
+      /AppBar> <
+      div className = {
+        classes.menu
+      } >
+      <
+      CustomerAdd stateRefresh = {
+        this.stateRefresh
+      }
+      /> <
+      /div> <
+      Paper className = {
+        classes.paper
+      } >
+      <
+      Table className = {
+        classes.table
+      } >
+      <
+      TableHead >
+      <
+      TableRow > {
+        cellList.map(c => {
+          return <TableCell className = {
+            classes.tableHead
+          } > {
+            c
+          } < /TableCell>
+        })
+      } <
+      /TableRow> <
+      /TableHead> <
+      TableBody > {
+        this.state.customers ?
+        filteredComponents(this.state.customers) :
+          <
+          TableRow >
+          <
+          TableCell colSpan = "6"
+        align = "center" >
+        <
+        CircularProgress className = {
+          classes.progress
+        }
+        variant = "determinate"
+        value = {
+          this.state.completed
+        }
+        /> <
+        /TableCell> <
+        /TableRow>
+      } <
+      /TableBody> <
+      /Table> <
+      /Paper> <
+      /div>
     );
   }
 }
